0d37cd6f2def28b1a9f73cd1b125e2eecf519eb1
iossenac <iossenac@SALA309PC05.local>
2018-05-19 14:42:32 -0300

    Trabalhando com Material - Temas e Cores, FAB, Card e Snackbar

---
+++ app/src/main/res/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.iossenac.appmaterialdesign;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Clicou no FAB", Snackbar.LENGTH_LONG)
+                        .setAction("Desfazer", new MyUndoListener()).show();
+            }
+        });
+    }
+
+    public class MyUndoListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+
+            Toast.makeText(MainActivity.this,"Disparado o botao Undo!",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+}
